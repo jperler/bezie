612cf5b86b59522c1fde024a1d3b6bf327162343
Justin Perler <jperler@gmail.com>
2016-08-26 00:17:39 -0400

    add constants

---
+++ app/constants.js
@@ -0,0 +1,17 @@
+export const colors = [
+    '#FF00FF',
+    '#00FFFF',
+    '#00FF00',
+    '#FF0000',
+    '#FFFF00',
+    '#9D00FF',
+]
+
+export const labels = [
+    '1 - Modulation',
+    '2 - Breath',
+    '3 - (Controller)',
+    '4 - Foot Pedal',
+    '5 - Portamento Time',
+    '6 - Data Entry',
+]
