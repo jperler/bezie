e98a416e84697feb8e75650be0a9947918858f10
Justin Perler <jperler@gmail.com>
2017-01-01 15:23:25 -0500

    remove native zoom

--- app/index.js
+++ app/index.js
@@ -3,12 +3,16 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { Router, hashHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
+import { webFrame } from 'electron'
 import routes from './routes'
 import configureStore from './store/configureStore'
 import './app.global.css'
 
 const store = configureStore()
 const history = syncHistoryWithStore(hashHistory, store)
+
+// Disable native broswer zoom
+webFrame.setZoomLevelLimits(1, 1)
 
 render(
     <Provider store={store}>
