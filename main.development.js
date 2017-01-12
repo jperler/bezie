1c57da383461af3164f70ff54209bea13a888f14
Justin Perler <jperler@gmail.com>
2017-01-11 23:50:42 -0500

    updated license watermark

--- main.development.js
+++ main.development.js
@@ -1,3 +1,29 @@
+/**!
+ *    License
+ *
+ *    Bezie grants you a revocable, non-exclusive, non-transferable, limited license to download,
+ *    install and use the Application solely for your personal, non-commercial purposes strictly in
+ *    accordance with the terms of this Agreement.
+ *
+ *    Restrictions
+ *
+ *    You agree not to, and you will not permit others to:
+ *
+ *    license, sell, rent, lease, assign, distribute, transmit, host, outsource, disclose or
+ *    otherwise commercially exploit the Application or make the Application available to any third
+ *    party. copy or use the Application for any purpose other than as permitted under the above
+ *    section 'License'. modify, make derivative works of, disassemble, decrypt, reverse compile or
+ *    reverse engineer any part of the Application. remove, alter or obscure any proprietary notice
+ *    (including any notice of copyright or trademark) of Bezie or its affiliates, partners,
+ *    suppliers or the licensors of the Application.
+ *
+ *    Intellectual Property
+ *
+ *    The Application, including without limitation all copyrights, patents, trademarks, trade
+ *    secrets and other intellectual property rights are, and shall remain, the sole and exclusive
+ *    property of Bezie.
+ */
+
 import { autoUpdater, app, BrowserWindow, Menu, shell, dialog } from 'electron'
 import os from 'os'
 import { RELEASE_BASE_URL } from './app/constants'
